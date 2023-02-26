d1b3cec21e08cf8e7f620b8d000ad9aaaa5c46fc
Nikit Soni <nikitsoni@Nikits-MacBook-Air.local>
2023-02-26 13:15:26 +0530

    Final changes done

---
+++ src/components/Clients.jsx
@@ -0,0 +1,16 @@
+import { clients } from "../constants";
+import styles from "../style";
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+          <img src={client.logo} alt="client_logo" className="sm:w-[120px] w-[90px] object-contain" />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Clients;
